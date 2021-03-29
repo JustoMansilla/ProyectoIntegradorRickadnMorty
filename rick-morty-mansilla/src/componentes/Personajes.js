575e4d1ad6b5bb588fb56ba4dd65cbd8784334b9
Justo Enrique Mansilla <jmansilla@udesa.edu.ar>
2021-03-29 15:49:59 -0300

    .

--- rick-morty-mansilla/src/componentes/Personajes.js
+++ rick-morty-mansilla/src/componentes/Personajes.js
@@ -4,20 +4,22 @@ function Personajes(props) {
     return(
         <React.Fragment> 
         
-            
             <img src={props.personaje.image} alt="" />
 
-            <h5>
+            <h4>
                 {props.personaje.name}
-            </h5>
+            </h4>
+
             <p>
             {props.personaje.status}
 
             </p>
+
             <p>
             {props.personaje.species}
 
             </p>
+
             <p>
                 Origin: 
             {props.personaje.origin.name}
